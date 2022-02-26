3187c660ffc7279242812e1edc1ed3de33647af8
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2022-02-26 15:06:36 +0800

    Update _install.sh

--- .travis/_install.sh
+++ .travis/_install.sh
@@ -5,7 +5,7 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-pipenv install --pypi-mirror https://pypi.python.org/simple
+pipenv install
 
 # install hexo commands
 npm install -g hexo-cli
