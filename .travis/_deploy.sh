e174d5980a01bcc8dc2f7ec0d15182a41cd76076
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2022-02-26 21:31:19 +0800

    [skip ci] update deploy script

--- .travis/_deploy.sh
+++ .travis/_deploy.sh
@@ -11,6 +11,7 @@ git config --global user.name "${GIT_USERNAME}"
 git clone --depth 1 --branch master --single-branch https://${GITHUB_PAT}@github.com/${BLOG_REPO}.git site
 cd site
 cp -r ../public/* ./
+cp -r ../.source/* ./
 ls
 NOW=$(date +"%Y-%m-%d %H:%M:%S %z")
 git add --all *
