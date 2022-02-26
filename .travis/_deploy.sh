f0d3d7e84bcde1a372db1e21b77697d909d07dc0
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2022-02-26 15:49:49 +0800

    Update _deploy.sh

--- .travis/_deploy.sh
+++ .travis/_deploy.sh
@@ -7,18 +7,12 @@ set -e
 
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_USERNAME}"
-
-if [ -d public ]; then
-
-    # deploy site to repo
-    git clone --depth 1 --branch master --single-branch https://${GITHUB_PAT}@github.com/${BLOG_REPO}.git site
-    cd site
-    cp -r ../public/* ./
-    ls
-
-    NOW=$(date +"%Y-%m-%d %H:%M:%S %z")
-    git add --all *
-    git commit -m "Site updated: ${NOW}" || true
-    git push -q origin master
-
-fi
+# deploy site to repo
+git clone --depth 1 --branch master --single-branch https://${GITHUB_PAT}@github.com/${BLOG_REPO}.git site
+cd site
+cp -r ../public/* ./
+ls
+NOW=$(date +"%Y-%m-%d %H:%M:%S %z")
+git add --all *
+git commit -m "Site updated: ${NOW}" || true
+git push -q origin master
